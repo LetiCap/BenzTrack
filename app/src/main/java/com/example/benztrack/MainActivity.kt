b78cac45deed03a5061906fef1226fed5ba6f65e
Crico1702 <cristian.codalonga@gmail.com>
2024-04-29 20:55:03 +0200

    funzionamento nav bar, manca l'implementazione corretta dei nomi per la nostra app e l'aggiunta di altri 2 tasti

--- app/src/main/java/com/example/benztrack/MainActivity.kt
+++ app/src/main/java/com/example/benztrack/MainActivity.kt
@@ -1,21 +1,46 @@
 package com.example.benztrack
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity()  {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
 
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> {
+                    replaceFragment(HomeFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.notification -> {
+                    replaceFragment(NotificationFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.setting -> {
+                    //replaceFragment(SettingFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                else -> return@setOnNavigationItemSelectedListener false
+            }
+        }
+
+        // Visualizza il frammento iniziale
+        replaceFragment(HomeFragment())
     }
 
+    // Funzione per sostituire il frammento nel contenitore del frammento
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment, fragment)
+            .commit()
+    }
 }
--- app/src/main/java/com/example/benztrack/NotificationFragment.kt
+++ app/src/main/java/com/example/benztrack/NotificationFragment.kt
@@ -6,18 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class HomeFragment : Fragment() {
+class NotificationFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_notification, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // You can initialize views and handle UI interactions here
+        // Puoi inizializzare le tue view e gestire le interazioni UI qui
     }
 }
