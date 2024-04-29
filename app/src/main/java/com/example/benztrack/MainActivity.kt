4aab4ee2b68894975e6e26c5479682466341b0c2
Crico1702 <cristian.codalonga@gmail.com>
2024-04-29 19:53:50 +0200

    introduzione nav bar con cambio fragment. selezionando i bottoni si visualizzera un nuovo fragment (funzione da sistemare).

--- app/src/main/java/com/example/benztrack/MainActivity.kt
+++ app/src/main/java/com/example/benztrack/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.example.benztrack
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
 
-
     }
+
 }
