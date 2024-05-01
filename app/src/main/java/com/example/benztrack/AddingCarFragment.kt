1ff5409e5f3f21f070d18c1cd92c87d3df9c8484
letiz <capitanioletizia@gamil.com>
2024-05-01 18:03:56 +0200

    creazione e rimozione di un fragment inutile. inseriemnto menu nell'adding car

--- app/src/main/java/com/example/benztrack/AddingCarFragment.kt
+++ app/src/main/java/com/example/benztrack/AddingCarFragment.kt
@@ -1,13 +1,14 @@
 package com.example.benztrack
 
-import android.app.VoiceInteractor
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
-import android.widget.EditText
+import android.widget.Spinner
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -15,14 +16,16 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-
 import okhttp3.OkHttpClient
 import okhttp3.Request
-import okio.IOException
+import java.io.IOException
 
 
 class AddingCarFragment : Fragment() {
-
+    private var selectedModel: String = ""
+    private var selectedAnno: String = ""
+    private var selectedType: String = ""
+    private var selectedMark: String = ""
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -33,10 +36,99 @@ class AddingCarFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_addingcar, container, false)
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val ModelloSpinner: Spinner = view.findViewById(R.id.Modello)
+        val TipoSpinner: Spinner = view.findViewById(R.id.Tipo)
+        val AnnoSpinner: Spinner = view.findViewById(R.id.Anno)
+        val MarchioSpinner: Spinner = view.findViewById(R.id.Marchio)
+        val btn = view.findViewById<Button>(R.id.btnAdd)
+        val outRes = view.findViewById<TextView>(R.id.result)
+
+
+        val ModellliList = listOf("Seleziona un Modello", "ProMaster City", "Moto", "Bicicletta")
+        val adapterModelloSpinner =
+            ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, ModellliList)
+        adapterModelloSpinner.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        ModelloSpinner.adapter = adapterModelloSpinner
+
+        val TipoList = listOf("Seleziona un Tipo", "Van/Minivan", "Moto", "Bicicletta")
+        val adapterTipoSpinner =
+            ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, TipoList)
+        adapterTipoSpinner.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        TipoSpinner.adapter = adapterTipoSpinner
+
+        val AnnoiList = listOf("Seleziona un Anno", "2019", "Moto", "Bicicletta")
+        val adapterAnnoSpinner =
+            ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, AnnoiList)
+        adapterAnnoSpinner.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        AnnoSpinner.adapter = adapterAnnoSpinner
+
+        val MarchioList = listOf("Seleziona un Marchio", "Ram", "Moto", "Bicicletta")
+        val adapterMarchioSpinner =
+            ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, MarchioList)
+        adapterMarchioSpinner.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        MarchioSpinner.adapter = adapterMarchioSpinner
+
+
+
+        // Imposta il listener per lo spinner Tipo
+        // Imposta il listener per lo spinner Marchio
+        // Imposta il listener per lo spinner Anno
+        // Imposta il listener per lo spinner Modello
+        TipoSpinner.onItemSelectedListener = createItemSelectedListener(TipoList, "Tipo")
+        MarchioSpinner.onItemSelectedListener = createItemSelectedListener(MarchioList, "Marchio")
+        AnnoSpinner.onItemSelectedListener = createItemSelectedListener(AnnoiList, "Anno")
+        ModelloSpinner.onItemSelectedListener = createItemSelectedListener(ModellliList, "Modello")
+
+
+        btn.setOnClickListener {
+            if(selectedType.isBlank() || selectedAnno.isBlank() || selectedMark.isBlank() || selectedModel.isBlank()) {
+                Toast.makeText(requireContext(), "inserire tutti i parametri", Toast.LENGTH_LONG)
+                    .show()
+            }else{fetchData(selectedType, selectedAnno, selectedMark, selectedModel, outRes)}
+
+
+
+        }
+
+
+
+    }
+
+
+    fun createItemSelectedListener(ListaPassata: List<String>, MessaggioPerMancatoInserimento: String): AdapterView.OnItemSelectedListener {
+        return object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                // Ignora la selezione se l'utente seleziona il suggerimento
+
+
+                    // Ottieni l'opzione selezionata
+                val selectedOption = ListaPassata[position]
+                if(position!=0) {
+                    when (MessaggioPerMancatoInserimento) {
+                        "Modello" -> selectedModel = selectedOption
+                        "Tipo" -> selectedType = selectedOption
+                        "Anno" -> selectedAnno = selectedOption
+                        "Marchio" -> selectedMark = selectedOption
+                    }
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // Azioni da eseguire quando non viene selezionata alcuna opzione
+            }
+        }
+    }
+}
+
+
+        /*
+
+
         var inAnno = view.findViewById<EditText>(R.id.Anno)
-        var inMarca = view.findViewById<EditText>(R.id.Marca)
+        var inMarca = view.findViewById<EditText>(R.id.Marchio)
         var inTipo = view.findViewById<EditText>(R.id.Tipo)
         var btn = view.findViewById<Button>(R.id.btnAdd)
         var outRes = view.findViewById<TextView>(R.id.result)
@@ -62,13 +154,15 @@ class AddingCarFragment : Fragment() {
             }
 
         }
-    }
+        */
 
-    private fun fetchData(tipo: String, anno: String, marca: String, testo: TextView) {
 
+
+    private fun fetchData(tipo: String?, anno: String?, marca: String?,modello: String?, testo: TextView) {
+        Log.d("fetchData", "Il valore di anno è: $anno")
         // Il resto del tuo codice per effettuare la chiamata di rete e gestire la risposta
-        val requestUrl =
-            "https://car-data.p.rapidapi.com/cars?limit=1&page=0&year=$anno&make=$marca&type=$tipo"
+        val requestUrl = "https://car-data.p.rapidapi.com/cars?limit=1&page=0&year=$anno&make=$marca&type=$tipo&model=$modello"
+       // "https://car-data.p.rapidapi.com/cars/years"
 
         // Esegui la chiamata API in un thread separato utilizzando coroutine
         GlobalScope.launch(Dispatchers.IO) {
@@ -103,4 +197,3 @@ class AddingCarFragment : Fragment() {
             }
         }
     }
-}
