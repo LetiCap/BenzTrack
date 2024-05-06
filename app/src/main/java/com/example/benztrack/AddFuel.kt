c1e7cd43cce1597b27aea46bbf8407191c08e16c
Crico1702 <cristian.codalonga@gmail.com>
2024-05-06 14:49:43 +0200

    inseriemento funzione per avere  la poszione attuale su mappa con puntatore, implementazione bottoni per ingrandire la mappa e risalire alla posizione attuale se ci si sposta sulla mappa

--- app/src/main/java/com/example/benztrack/AddFuel.kt
+++ app/src/main/java/com/example/benztrack/AddFuel.kt
@@ -1,23 +1,34 @@
 package com.example.benztrack
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
-
-
+import com.google.android.gms.maps.model.MarkerOptions
 
 
 class AddFuel : Fragment(), OnMapReadyCallback {
 
     private lateinit var mapView: MapView
     private lateinit var googleMap: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    //codice per la richiesta del permesso
+    private val LOCATION_PERMISSION_REQUEST_CODE = 1001
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,8 +49,78 @@ class AddFuel : Fragment(), OnMapReadyCallback {
 
     override fun onMapReady(map: GoogleMap) {
         googleMap = map
-        val initialLocation = LatLng(40.7128, -74.0060)
+        val initialLocation = LatLng(49.7128, -74.0060)
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(initialLocation, 10f))
+
+        // Inizializza il client per ottenere la posizione
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
+
+        // Ottieni la posizione attuale e aggiungi un marker sulla mappa
+        // Inizializza il client per ottenere la posizione
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
+
+        // Richiedi il permesso di accesso alla posizione
+        requestLocationPermission()
+    }
+
+    // Funzione per richiedere il permesso di accesso alla posizione
+    private fun requestLocationPermission() {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // Se il permesso non è stato concesso, richiedilo all'utente
+            requestPermissions(
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
+        } else {
+            // Se il permesso è già stato concesso, ottieni la posizione attuale
+            getCurrentLocation()
+        }
+    }
+
+
+    // Funzione per ottenere la posizione attuale dell'utente
+    private fun getCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            return
+        }
+        fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+            location?.let {
+                // Aggiungi un marker sulla mappa nella posizione attuale
+                val currentPosition = LatLng(location.latitude, location.longitude)
+                googleMap.addMarker(
+                    MarkerOptions().position(currentPosition).title("La mia posizione attuale")
+                )
+                // Muovi la camera per centrare il marker
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentPosition, 15f))
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Se il permesso è stato concesso, ottieni la posizione attuale
+                getCurrentLocation()
+            } else {
+                //toast per avviso permesso negato
+            }
+        }
     }
 
     override fun onResume() {
